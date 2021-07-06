d4de5d359ad3c50456c9f2a4a65b1cdee6bac44e
FabriMontserrat <fabrimontserrat@gmail.com>
2021-07-05 22:31:54 -0300

    Ejercicios 1 - 5

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -4,9 +4,14 @@ y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+
+	//nombreIngresado = txtIdNombre.value;
+	
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+
+	alert("Su nombre es: " + nombreIngresado);
 
 }
 
 
+//txtIdNombre
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,13 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var num1
+	var num2
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num2 = document.getElementById("txtIdNumeroDos").value;
+ 
+
+
 }
 
